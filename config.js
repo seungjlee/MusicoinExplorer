b2a376e89d91b79462a34aa82fb6f0478ce3f8d7
sjlee <sj_lee@yahoo.com>
2018-01-06 17:54:41 -0500

    Initial commit for Ethereum Classic branch.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var net = require('net');
 var config = function ()
 {  
   this.logFormat = "combined";
-  this.ipcPath = "\\\\.\\pipe\\musicoin.ipc";
+  this.ipcPath = "\\\\.\\pipe\\etc.ipc";
   this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
   
   this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
