0851394562cbfdce62a08747e1a8f1be5592aa9c
sjlee <sj_lee@yahoo.com>
2018-01-11 05:05:04 -0500

    Names for some addresses.

--- config.js
+++ config.js
@@ -11,7 +11,13 @@ var config = function ()
   
   this.names =
   {
-    "0x0068a648c25b93b46408f8ba2e3952774bab54b1": "Developer SJL"
+    "0x0068a648c25b93b46408f8ba2e3952774bab54b1": "Developer SJL",
+    "0x6dbfe39370adc9e0f284ed4fd8025342e99d21d6": "dwarfpool.com",
+    "0x88059a92c6a5777e015b432b11120abc26ae8bfe": "expmine.pro",
+    "0xbab463743603a253bdf1f84975b1a9517505ae05": "expanse.miningpoolhub.com",
+    "0x935115b9ae16338a6378549203476cbefb97923f": "pool.expanse.tech",
+    "0x4a870254919ace6bf7f1dc80fe6de28299beb6d9": "exp.minerpool.net",
+    "0x94c4b0f47123ad4d3dec4c27d96eb4061aa544e0": "exp.ethertrench.com"
   }
   
   this.sslKey = "";
